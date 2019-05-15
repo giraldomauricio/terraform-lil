6cd7cf4d9f17118a3384899d68a9ff5c8cf12e31
giraldomauricio <mgiraldo@gmail.com>
2019-05-15 00:19:07 -0400

    Added AWS Subnets

--- resources.tf
+++ resources.tf
@@ -7,3 +7,26 @@ resource "aws_vpc" "aws-environment-example" {
     Name = "terraform-aws-vpc-example"
   }
 }
+
+resource "aws_subnet" "subnet1" {
+  cidr_block        = "${cidrsubnet(aws_vpc.aws-environment-example.cidr_block,3,1)}"
+  vpc_id            = "${aws_vpc.aws-environment-example.id}"
+  availability_zone = "us-west-2a"
+}
+
+resource "aws_subnet" "subnet2" {
+  cidr_block        = "${cidrsubnet(aws_vpc.aws-environment-example.cidr_block,2,2)}"
+  vpc_id            = "${aws_vpc.aws-environment-example.id}"
+  availability_zone = "us-west-2b"
+}
+
+resource "aws_security_group" "subnetsecurity" {
+  vpc_id = "$aws_vpc.aws-environment-example.id"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["${aws_vpc.aws-environment-example.cidr_block}"]
+  }
+}
